08c840ae3e9a5ff1aec30ae0ada76e4ece08e34b
shakibul22 <shakibulislam684@gmail.com>
2023-12-09 07:26:42 +0600

    banner is added

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Banner.css'; // Import your CSS file
 
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="banner">
+      <div className="banner-content">
+        <h1>Welcome to Our Website</h1>
+        <p>Explore our amazing services!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
